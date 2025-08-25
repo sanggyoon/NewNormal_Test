e6a7c90453fa50d72d9352c32a5a87a232822b3e
sanggyoon <sangreal99@naver.com>
2025-08-25 16:11:27 +0900

    feat: 5초마다 데이터 업데이트

--- Frontend/JS/gauge.js
+++ Frontend/JS/gauge.js
@@ -1,3 +1,60 @@
+window.liveGauges = {
+  0: {},
+  1: {},
+  2: {},
+  3: {},
+};
+
+function getGradientByValue(value, maxValue) {
+  if (value <= 1) return ['#007BFE', '#007BFE'];
+  else if (value <= 2) return ['#FFD600', '#FFD600'];
+  else return ['#EE5253', '#EE5253'];
+}
+
+const gasKeys = ['avg1', 'avg2', 'avg3', 'avg4'];
+const gasNames = ['NH3', 'H2S', 'CH4', 'CO2'];
+const canvasIds = [
+  'ammoniaGauge_',
+  'hydrogenSulfideGauge_',
+  'methaneGauge_',
+  'carbonDioxideGauge_',
+];
+
+for (let i = 0; i < 4; i++) {
+  const data = DummyData.items[i];
+
+  gasKeys.forEach((key, idx) => {
+    document.querySelectorAll(`#${canvasIds[idx]}${i}`).forEach((canvas) => {
+      const value = data[key];
+      const [startColor, stopColor] = getGradientByValue(value, 3.0);
+      const opts = {
+        angle: 0.12,
+        lineWidth: 0.44,
+        radiusScale: 1,
+        pointer: {
+          length: 0.62,
+          strokeWidth: 0.08,
+          color: '#000000',
+        },
+        limitMax: false,
+        limitMin: false,
+        strokeColor: '#E0E0E0',
+        generateGradient: true,
+        highDpiSupport: true,
+        colorStart: startColor,
+        colorStop: stopColor,
+      };
+      const gauge = new Gauge(canvas).setOptions(opts);
+      gauge.maxValue = 3.0;
+      gauge.setMinValue(0);
+      gauge.animationSpeed = 64;
+      gauge.set(value);
+      window.liveGauges[i][gasNames[idx]] = gauge; // 저장
+    });
+  });
+}
+
+// 값에 따른 게이지 스타일
 function getGradientByValue(value, maxValue) {
   if (value <= 1) {
     return ['#007BFE', '#007BFE']; // 파란색
